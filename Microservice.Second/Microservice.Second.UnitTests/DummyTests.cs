c4fdb7598758cf8fbe0ea6a94175efe8343ce1ed
de_fault <alexizual1@gmail.com>
2020-10-19 13:48:43 +0500

    add tests and fix pipline

---
+++ Microservice.Second/Microservice.Second.UnitTests/DummyTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Microservice.Second.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Dummy_Always_Pass()
+        {
+            Assert.Pass();
+        }
+    }
+}
