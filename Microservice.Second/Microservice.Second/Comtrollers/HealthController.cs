ddf4ffcb1d5bd13c3a38207d19c1ac90c4e0efd0
de_fault <alexizual1@gmail.com>
2020-10-20 14:15:51 +0500

    change second service

---
+++ Microservice.Second/Microservice.Second/Comtrollers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Microservice.Second.Comtrollers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        [Route("healthcheck")]
+        public IActionResult HealthCheck()
+        {
+            return Ok();
+        }
+    }
+}
